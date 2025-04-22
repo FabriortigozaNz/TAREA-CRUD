37e367747574d95d7d7c3650ac986e16020c7d2d
FabriortigozaNz <fabricioortigoza3@gmail.com>
2025-04-21 22:43:08 -0300

    2: Define el esquema de Mongoose para productos

--- productos.model.ts
+++ productos.model.ts
@@ -1,1 +1,15 @@
 // Aquí se definirán los campos y validaciones del modelo de producto.
+
+import mongoose from "mongoose";
+
+// Definimos el esquema de los documentos "Producto"
+const ProductoSchema = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  descripcion: { type: String },
+  precio: { type: Number, default: 0, required: true },
+  stock: { type: Number, default: 0 },
+  disponible: { type: Boolean, default: true },
+});
+
+// Creamos y exportamos el modelo
+export const Producto = mongoose.model("Producto", ProductoSchema);
