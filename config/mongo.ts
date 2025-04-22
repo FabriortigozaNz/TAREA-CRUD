6d941b8279ce98f0b6c19d5825c0966a39cbfee1
FabriortigozaNz <fabricioortigoza3@gmail.com>
2025-04-21 22:52:53 -0300

    5: Crea la conexión a MongoDB en mongo.ts

--- config/mongo.ts
+++ config/mongo.ts
@@ -1,1 +1,16 @@
 // Conexion con la base de datos MongoDB (pendiente)
+
+import mongoose from "mongoose";
+
+// Asegúrate de reemplazar esta URL con tu propia URL de conexión si es necesario
+const mongoURI = "mongodb://localhost:27017/tienda";
+
+export const conectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Conexión a MongoDB exitosa");
+  } catch (error) {
+    console.error("Error de conexión a MongoDB:", error);
+    
+  }
+};
